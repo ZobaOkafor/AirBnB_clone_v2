2915e12b4e5bb7d90a828a47cbc1dd834bc38c8e
ZobaOkafor <judithokafor30@gmail.com>
2024-04-04 05:56:47 -0700

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers,
-using the function do_deploy.
-"""
-
-from fabric.api import env, put, run
-from os.path import exists
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import *
+import shlex
+import os
 
-env.hosts = ['54.152.235.22', '35.175.64.89']
-env.user = 'ubuntu'
+
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers."""
-    if not exists(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
-
     try:
-        # Upload the archive to /tmp/ directory
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-
-        # Extract archive to /data/web_static/releases/
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-        full_path = "/data/web_static/releases/{}/".format(foldername)
-        run("mkdir -p {}".format(full_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, full_path))
-
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
-
-        # Move contents of extracted folder to its parent folder
-        run("mv {}web_static/* {}".format(full_path, full_path))
-
-        # Remove extracted folder
-        run("rm -rf {}web_static".format(full_path))
-
-        # Delete the symbolic link /data/web_static/current
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run("ln -s {} /data/web_static/current".format(full_path))
-
+        run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except Exception as e:
-        print(e)
+    except:
         return False
