ce34e9f4cb082bb5fed393867b301517a433cb5c
ZobaOkafor <judithokafor30@gmail.com>
2024-04-04 05:15:00 -0700

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,19 @@ Fabric script that distributes an archive to your web servers,
 using the function do_deploy.
 """
 
-from fabric.api import env, put, run
-from os.path import exists
+from fabric.context_managers import cd, hide,\
+        settings, show, path, prefix, lcd, quiet, warn_only,\
+        remote_tunnel, shell_env
+from fabric.decorators import hosts, roles,\
+        runs_once, with_settings, task, serial, parallel
+from fabric.operations import require, prompt,\
+        put, get, run, sudo, local, reboot, open_shell
+from fabric.state import env, output
+from fabric.utils import abort, warn, puts, fastprint
+from fabric.tasks import execute
 from datetime import datetime
+import os
+
 
 env.hosts = ['54.152.235.22', '35.175.64.89']
 env.user = "ubuntu"
