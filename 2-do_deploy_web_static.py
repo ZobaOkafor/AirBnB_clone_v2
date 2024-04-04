706efe4adfc270ba80a67a56261bae441c2f789e
ZobaOkafor <judithokafor30@gmail.com>
2024-04-04 04:55:47 -0700

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,40 +10,52 @@ from datetime import datetime
 
 env.hosts = ['54.152.235.22', '35.175.64.89']
 env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa.pub'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers."""
+    """
+    Function that distributes an archive to web servers
+    """
     if not exists(archive_path):
         return False
 
     try:
+        # Extract file name and directory name from archive_path
+        filename = archive_path.split('/')[-1]
+        directory_name = filename.split('.')[0]
+
+        # Remote paths
+        remote_tmp_path = "/tmp/"
+        remote_release_path = "/data/web_static/releases/{}/"
+        .format(directory_name)
+
         # Upload the archive to /tmp/ directory
-        put(archive_path, "/tmp/")
+        put(archive_path, remote_tmp_path)
+
+        # Create necessary directories
+        run("mkdir -p {}".format(remote_release_path))
 
         # Extract archive to /data/web_static/releases/
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-        full_path = "/data/web_static/releases/{}/".format(foldername)
-        run("mkdir -p {}".format(full_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, full_path))
+        run("tar -xzf {}{} -C {}"
+            .format(remote_tmp_path, filename, remote_release_path))
 
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
+        # Remove the uploaded archive from the web server
+        run("rm {}{}".format(remote_tmp_path, filename))
 
         # Move contents of extracted folder to its parent folder
-        run("mv {}web_static/* {}".format(full_path, full_path))
+        run("mv {}web_static/* {}"
+            .format(remote_release_path, remote_release_path))
 
         # Remove extracted folder
-        run("rm -rf {}web_static".format(full_path))
+        run("rm -rf {}web_static".format(remote_release_path))
 
         # Delete the symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link
-        run("ln -s {} /data/web_static/current".format(full_path))
+        run("ln -s {} /data/web_static/current".format(remote_release_path))
 
-        print("New version deployed!")
         return True
     except Exception as e:
         print(e)
