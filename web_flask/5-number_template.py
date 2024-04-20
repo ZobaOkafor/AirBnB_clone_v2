a5f150b2a18a2e6e273be706bbd3bfc3e096ee3e
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 12:16:39 -0700

    Start a flask web application

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -13,7 +13,8 @@ Routes:
     /number_template/<n>: display a HTML page only if n is an integer:
     H1 tag: “Number: n” inside the tag BODY
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
