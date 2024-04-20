f72e1a91448968032fa2b5c141b6c108fdbf09e2
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 14:06:48 -0700

    Start a flask web application

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+
+Routes:
+    /hbnb: HBnB home page.
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+    return render_template("100-hbnb.html",
+                           states=states, amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
