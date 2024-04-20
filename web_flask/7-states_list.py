9e5f8e9441047c5e7be64baf81528c1e09bf823a
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 14:32:34 -0700

    Start a flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
 Script that starts a Flask web application
+
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
 """
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
@@ -13,9 +15,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with the list of states"""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states_sorted)
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
