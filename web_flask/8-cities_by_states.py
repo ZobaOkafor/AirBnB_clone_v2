21c1d6f0fc903b8cc760c6ac726e8b86ecbef612
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 13:37:35 -0700

    Start a flask web application

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+
+Routes:
+    /cities_by_states: HTML page with a list of all states and
+    related cities.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with the list of states and their cities"""
+    states = generate_states()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
