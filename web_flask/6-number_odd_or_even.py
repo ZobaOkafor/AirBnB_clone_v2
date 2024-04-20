7d3f78cb79b48cee01c99eed25f6c7adddcd4ad4
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 12:36:47 -0700

    Start a flask web application

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -9,12 +9,13 @@ Routes:
     /python/(<text>): display “Python ”, followed by the value of the
     text variable (replace underscore _ symbols with a space).
     The default value of text is “is cool”
-    /number/<n>: display “n is a number” only if n is an integer.
+    /number/<n>: display “n is a number” only if n is an integer
     /number_template/<n>: display a HTML page only if n is an integer:
     H1 tag: “Number: n” inside the tag BODY
+    /number_odd_or_even/<n>: display a HTML page only if n is an integer:
+    H1 tag: “Number: n is even|odd” inside the tag BODY
 """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -53,7 +54,13 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """Display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_number_odd_or_even(n):
+    """Display a HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', number=n, odd_even='odd' if n % 2 != 0 else 'even')
 
 
 if __name__ == '__main__':
