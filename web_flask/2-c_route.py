990d81312481132795c0228e59165929e4ce5aaa
ZobaOkafor <judithokafor30@gmail.com>
2024-04-20 11:22:46 -0700

    Start a flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -27,7 +27,7 @@ def display_hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     """Display 'C' followed by the value of the text variable"""
-    return 'C {}'.format(text)
+    return 'C {}'.format(escape(text).replace('_', ' '))
 
 
 if __name__ == '__main__':
